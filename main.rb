387940386bf80d15633c43f768b70f21a0995187
ruchika <ruchiika@gmail.com>
2015-05-19 01:41:57 -0700

    initial commit

---
+++ main.rb
@@ -0,0 +1,51 @@
+require 'sinatra'
+require 'slim'
+require 'sass'
+require './student'
+
+configure do
+  enable :sessions
+  set :username, 'frank'
+  set :password, 'sinatra'
+end
+
+configure :development do
+  DataMapper.setup(:default, "sqlite3://#{Dir.pwd}/student.db")
+end
+
+get('/styles.css'){ scss :styles }
+
+get '/' do
+  slim :login
+end
+
+get '/home' do  
+  slim :home
+end
+
+get '/about' do
+  @title = "All About This Website"
+  slim :about
+end
+
+get '/contact' do
+  slim :contact
+end
+
+not_found do
+  slim :not_found
+end
+
+post '/login' do
+  if params[:username] == settings.username && params[:password] == settings.password
+    session[:admin] = true
+    redirect to('/home')
+  else
+    slim :login
+  end
+end
+
+get '/logout' do
+  session.clear
+  redirect to('/')
+end
